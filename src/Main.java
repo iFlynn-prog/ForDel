75f5b18c418fb6f8fe5bd1e61f8109d6b2dd845b
iFlynn-prog <minibikk@gmail.com>
2023-12-15 16:07:54 +0300

    Calkulator

--- src/Main.java
+++ src/Main.java
@@ -5,49 +5,71 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         boolean request = true;
+        int a = 0, b = 0;
 
-        try {
-            while (request) {
-                request = false;
+        while (request) {
+            request = false;
+
+            try {
                 System.out.println("Введите первое число");
-                int a = sc.nextInt();
+                a = Integer.parseInt(sc.next());
+            } catch (NumberFormatException num) {
+                System.out.println("Введите только числовое значение");
+                break;
+            }
+
+            try {
                 System.out.println("Введите второе число");
-                int b = sc.nextInt();
+                b = Integer.parseInt(sc.next());
+            } catch (NumberFormatException num) {
+                System.out.println("Введите только числовое значение");
+                break;
+            }
 
-                System.out.println("Выберите вариант: +, -, /, *");
-                switch (sc.next()) {
-                    case "+":
-                        System.out.println("Сложение");
-                        System.out.println(a + b);
-                        break;
-                    case "-":
-                        System.out.println("Вычитание");
-                        System.out.println(a - b);
-                        break;
-                    case "/":
-                        System.out.println("Деление");
-                        System.out.println(a / b);
-                        break;
-                    case "*":
-                        System.out.println("Умножение");
-                        System.out.println(a * b);
-                        break;
-                    default:
-                        System.out.println("Выберите только: +, -, /, * ");
+            System.out.println("Выберите вариант: +, -, /, *");
+            switch (sc.next()) {
+                case "+":
+                    System.out.println("Сложение");
+                    System.out.println(a + b);
+                    break;
+                case "-":
+                    System.out.println("Вычитание");
+                    System.out.println(a - b);
+                    break;
+                case "/":
+                    System.out.println("Деление");
+                    System.out.println(a / b);
+                    break;
+                case "*":
+                    System.out.println("Умножение");
+                    System.out.println(a * b);
+                    break;
+                default:
+                    System.out.println("Выберите только: +, -, /, * ");
+                    //TODO ТОже самое что и в прошлом ТУДУ
+                    request = true;
+            }
 
-                }
-                System.out.println("Продолжаем?");
+            System.out.println("Продолжаем? Выберите Y/N");
+
+            try {
                 String choose = sc.next();
+
                 if (choose.equalsIgnoreCase("y")) {
                     request = true;
                 } else if (choose.equalsIgnoreCase("n")) {
-                    System.out.println("Покеда");
+                    System.out.println("Программа завершена");
                     break;
+                } else {
+                    System.out.println("Не выбрано действие Y/N");
+                    //TODO Заменить выражение для повторного опроса,
+                    // а не для перезапуска всего цикла программы
+                    request = true;
                 }
-            }
 
-        } catch (InputMismatchException ar) {
-            System.out.println("Ошибка ввода, проверьте ввод");
+            } catch (InputMismatchException ar) {
+                System.out.println("Ошибка ввода, проверьте ввод");
+            }
         }
     }
 }
